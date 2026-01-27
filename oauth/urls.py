1af93f666d20a15a27d8b14987eb5fcbe8586f9d
_kagema <137799358+KagemaNjoroge@users.noreply.github.com>
2026-01-27 19:10:19 +0300

    refactor: clean up URL patterns in oauth/urls.py for consistency

--- oauth/urls.py
+++ oauth/urls.py
@@ -4,22 +4,15 @@ from . import views
 
 app_name = "oauth"
 urlpatterns = [
+    path(r"oauth/authorize", views.authorize),
     path(
-        r'oauth/authorize',
-        views.authorize),
-    path(
-        r'oauth/requireemail/<int:oauthid>.html',
+        r"oauth/requireemail/<int:oauthid>",
         views.RequireEmailView.as_view(),
-        name='require_email'),
+        name="require_email",
+    ),
     path(
-        r'oauth/emailconfirm/<int:id>/<sign>.html',
-        views.emailconfirm,
-        name='email_confirm'),
-    path(
-        r'oauth/bindsuccess/<int:oauthid>.html',
-        views.bindsuccess,
-        name='bindsuccess'),
-    path(
-        r'oauth/oauthlogin',
-        views.oauthlogin,
-        name='oauthlogin')]
+        r"oauth/emailconfirm/<int:id>/<sign>", views.emailconfirm, name="email_confirm"
+    ),
+    path(r"oauth/bindsuccess/<int:oauthid>", views.bindsuccess, name="bindsuccess"),
+    path(r"oauth/oauthlogin", views.oauthlogin, name="oauthlogin"),
+]
