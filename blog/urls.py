f8d80975417e4c9e27a1692f046035de3d37e113
_kagema <137799358+KagemaNjoroge@users.noreply.github.com>
2026-01-27 19:08:01 +0300

    refactor: standardize URL patterns by removing unnecessary file extensions and improving readability

--- blog/urls.py
+++ blog/urls.py
@@ -5,58 +5,45 @@ from . import views
 
 app_name = "blog"
 urlpatterns = [
+    path(r"", views.IndexView.as_view(), name="index"),
+    path(r"page/<int:page>/", views.IndexView.as_view(), name="index_page"),
     path(
-        r'',
-        views.IndexView.as_view(),
-        name='index'),
-    path(
-        r'page/<int:page>/',
-        views.IndexView.as_view(),
-        name='index_page'),
-    path(
-        r'article/<int:year>/<int:month>/<int:day>/<int:article_id>.html',
+        r"article/<int:year>/<int:month>/<int:day>/<int:article_id>",
         views.ArticleDetailView.as_view(),
-        name='detailbyid'),
+        name="detailbyid",
+    ),
     path(
-        r'category/<slug:category_name>.html',
+        r"category/<slug:category_name>",
         views.CategoryDetailView.as_view(),
-        name='category_detail'),
+        name="category_detail",
+    ),
     path(
-        r'category/<slug:category_name>/<int:page>.html',
+        r"category/<slug:category_name>/<int:page>",
         views.CategoryDetailView.as_view(),
-        name='category_detail_page'),
+        name="category_detail_page",
+    ),
     path(
-        r'author/<author_name>.html',
+        r"author/<author_name>",
         views.AuthorDetailView.as_view(),
-        name='author_detail'),
+        name="author_detail",
+    ),
     path(
-        r'author/<author_name>/<int:page>.html',
+        r"author/<author_name>/<int:page>",
         views.AuthorDetailView.as_view(),
-        name='author_detail_page'),
+        name="author_detail_page",
+    ),
+    path(r"tag/<slug:tag_name>", views.TagDetailView.as_view(), name="tag_detail"),
     path(
-        r'tag/<slug:tag_name>.html',
+        r"tag/<slug:tag_name>/<int:page>",
         views.TagDetailView.as_view(),
-        name='tag_detail'),
+        name="tag_detail_page",
+    ),
     path(
-        r'tag/<slug:tag_name>/<int:page>.html',
-        views.TagDetailView.as_view(),
-        name='tag_detail_page'),
-    path(
-        'archives.html',
-        cache_page(
-            60 * 60)(
-            views.ArchivesView.as_view()),
-        name='archives'),
-    path(
-        'links.html',
-        views.LinkListView.as_view(),
-        name='links'),
-    path(
-        r'upload',
-        views.fileupload,
-        name='upload'),
-    path(
-        r'clean',
-        views.clean_cache_view,
-        name='clean'),
+        "archives",
+        cache_page(60 * 60)(views.ArchivesView.as_view()),
+        name="archives",
+    ),
+    path("links", views.LinkListView.as_view(), name="links"),
+    path(r"upload", views.fileupload, name="upload"),
+    path(r"clean", views.clean_cache_view, name="clean"),
 ]
