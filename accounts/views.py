652a62127822231c73543e98ed0a81bc27eb692a
_kagema <137799358+KagemaNjoroge@users.noreply.github.com>
2026-03-24 08:26:48 +0300

    refactor: translate chinese comments to english for readability

--- accounts/views.py
+++ accounts/views.py
@@ -16,7 +16,13 @@ from django.urls import reverse
 from django.utils.http import url_has_allowed_host_and_scheme
 from django.views import View
 
-from djangoblog.utils import send_email, get_sha256, get_current_site, generate_code, delete_sidebar_cache
+from djangoblog.utils import (
+    send_email,
+    get_sha256,
+    get_current_site,
+    generate_code,
+    delete_sidebar_cache,
+)
 from djangoblog.base_views import SecureFormView, LoginFormView, LogoutRedirectView
 from . import utils
 from .forms import RegisterForm, LoginForm, ForgetPasswordForm, ForgetPasswordCodeForm
@@ -25,94 +31,96 @@ from .models import BlogUser
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
-
 class RegisterView(SecureFormView):
     """
-    用户注册视图（重构版）
+    User registration view (reconstructed version)
 
-    使用 SecureFormView 基类，自动提供 CSRF 保护
+    Using the SecureFormView base class, CSRF protection is automatically provided.
     """
+
     form_class = RegisterForm
-    template_name = 'account/registration_form.html'
+    template_name = "account/registration_form.html"
 
     def form_valid(self, form):
         if form.is_valid():
             user = form.save(False)
             user.is_active = False
-            user.source = 'Register'
+            user.source = "Register"
             user.save(True)
             site = get_current_site().domain
             sign = get_sha256(get_sha256(settings.SECRET_KEY + str(user.id)))
 
             if settings.DEBUG:
-                site = '127.0.0.1:8000'
-            path = reverse('account:result')
+                site = "127.0.0.1:8000"
+            path = reverse("account:result")
             url = "http://{site}{path}?type=validation&id={id}&sign={sign}".format(
-                site=site, path=path, id=user.id, sign=sign)
+                site=site, path=path, id=user.id, sign=sign
+            )
 
             content = """
-                            <p>请点击下面链接验证您的邮箱</p>
+                            <p>Please click the link below to verify your email address.</p>
 
                             <a href="{url}" rel="bookmark">{url}</a>
 
-                            再次感谢您！
+                            Thank you!
                             <br />
-                            如果上面链接无法打开，请将此链接复制至浏览器。
+                            If the link above cannot be opened, please copy this link into your browser.
                             {url}
-                            """.format(url=url)
+                            """.format(
+                url=url
+            )
             send_email(
                 emailto=[
                     user.email,
                 ],
-                title='验证您的电子邮箱',
-                content=content)
+                title="Verify your email address",
+                content=content,
+            )
 
-            url = reverse('accounts:result') + \
-                  '?type=register&id=' + str(user.id)
+            url = reverse("accounts:result") + "?type=register&id=" + str(user.id)
             return HttpResponseRedirect(url)
         else:
-            return self.render_to_response({
-                'form': form
-            })
+            return self.render_to_response({"form": form})
 
 
 class LogoutView(LogoutRedirectView):
     """
-    用户登出视图（重构版）
+    User Logout View (Refactored)
 
-    使用 LogoutRedirectView 基类，自动禁用缓存
+    Uses the LogoutRedirectView base class, automatically disabling caching.
     """
-    url = '/login/'
+
+    url = "/login/"
 
     def get(self, request, *args, **kwargs):
         logout(request)
         delete_sidebar_cache()
-        # 获取响应对象并删除登录标记 cookie
+        # Retrieve the response object and delete the login cookie.
         response = super(LogoutView, self).get(request, *args, **kwargs)
-        response.delete_cookie('logged_user')
+        response.delete_cookie("logged_user")
         return response
 
 
 class LoginView(LoginFormView):
     """
-    用户登录视图（重构版）
+    User Login View (Refactored Version)
 
-    使用 LoginFormView 基类，自动提供：
-    - 敏感参数保护（password）
-    - CSRF 保护
-    - 禁用缓存
+    Uses the LoginFormView base class, automatically providing:
+    - Sensitive parameter protection (password)
+    - CSRF protection
+    - Disable caching
     """
+
     form_class = LoginForm
-    template_name = 'account/login.html'
-    success_url = '/'
+    template_name = "account/login.html"
+    success_url = "/"
     redirect_field_name = REDIRECT_FIELD_NAME
 
     def get_context_data(self, **kwargs):
         redirect_to = self.request.GET.get(self.redirect_field_name)
         if redirect_to is None:
-            redirect_to = '/'
-        kwargs['redirect_to'] = redirect_to
+            redirect_to = "/"
+        kwargs["redirect_to"] = redirect_to
 
         return super(LoginView, self).get_context_data(**kwargs)
 
@@ -124,91 +132,91 @@ class LoginView(LoginFormView):
             logger.info(self.redirect_field_name)
 
             auth.login(self.request, form.get_user())
-            # 设置登录有效期
+            # Set login validity period
             if self.request.POST.get("remember"):
                 self.request.session.set_expiry(settings.REMEMBER_ME_LOGIN_TTL)
                 cookie_max_age = settings.REMEMBER_ME_LOGIN_TTL
             else:
-                # 使用Django默认的2周
+                # Using Django's default 2 weeks
                 self.request.session.set_expiry(settings.SESSION_COOKIE_AGE)
                 cookie_max_age = settings.SESSION_COOKIE_AGE
 
-            # 获取响应对象并设置登录标记 cookie
+            # Retrieve the response object and set the login cookie.
             response = super(LoginView, self).form_valid(form)
             response.set_cookie(
-                'logged_user',
-                'true',
+                "logged_user",
+                "true",
                 max_age=cookie_max_age,
-                httponly=False,  # 允许 JavaScript 访问
-                samesite='Lax'
+                httponly=False,  # Allow JavaScript access
+                samesite="Lax",
             )
             return response
             # return HttpResponseRedirect('/')
         else:
-            return self.render_to_response({
-                'form': form
-            })
+            return self.render_to_response({"form": form})
 
     def get_success_url(self):
 
         redirect_to = self.request.POST.get(self.redirect_field_name)
         if not url_has_allowed_host_and_scheme(
-                url=redirect_to, allowed_hosts=[
-                    self.request.get_host()]):
+            url=redirect_to, allowed_hosts=[self.request.get_host()]
+        ):
             redirect_to = self.success_url
         return redirect_to
 
 
 def account_result(request):
-    type = request.GET.get('type')
-    id = request.GET.get('id')
+    type = request.GET.get("type")
+    id = request.GET.get("id")
 
     user = get_object_or_404(get_user_model(), id=id)
     logger.info(type)
     if user.is_active:
-        return HttpResponseRedirect('/')
-    if type and type in ['register', 'validation']:
-        if type == 'register':
-            content = '''
-    恭喜您注册成功，一封验证邮件已经发送到您的邮箱，请验证您的邮箱后登录本站。
-    '''
-            title = '注册成功'
+        return HttpResponseRedirect("/")
+    if type and type in ["register", "validation"]:
+        if type == "register":
+            content = """
+  Congratulations on your successful registration! A verification email has been sent to your inbox. Please verify your email address and log in to this site.
+    """
+            title = "Registration successful"
         else:
             c_sign = get_sha256(get_sha256(settings.SECRET_KEY + str(user.id)))
-            sign = request.GET.get('sign')
+            sign = request.GET.get("sign")
             if sign != c_sign:
                 return HttpResponseForbidden()
             user.is_active = True
             user.save()
-            content = '''
-            恭喜您已经成功的完成邮箱验证，您现在可以使用您的账号来登录本站。
-            '''
-            title = '验证成功'
-        return render(request, 'account/result.html', {
-            'title': title,
-            'content': content
-        })
+            content = """
+           Congratulations! You have successfully completed email verification. You can now log in to this site using your account.
+            """
+            title = "Verification successful"
+        return render(
+            request, "account/result.html", {"title": title, "content": content}
+        )
     else:
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect("/")
 
 
 class ForgetPasswordView(SecureFormView):
     """
-    忘记密码视图（重构版）
+    Forgot Password View (Refactored)
 
-    使用 SecureFormView 基类，自动提供 CSRF 保护
+    Uses the SecureFormView base class, automatically providing CSRF protection.
     """
+
     form_class = ForgetPasswordForm
-    template_name = 'account/forget_password.html'
+    template_name = "account/forget_password.html"
 
     def form_valid(self, form):
         if form.is_valid():
-            blog_user = BlogUser.objects.filter(email=form.cleaned_data.get("email")).get()
+            blog_user = BlogUser.objects.filter(
+                email=form.cleaned_data.get("email")
+            ).get()
             blog_user.password = make_password(form.cleaned_data["new_password2"])
             blog_user.save()
-            return HttpResponseRedirect('/login/')
+            return HttpResponseRedirect("/login/")
         else:
-            return self.render_to_response({'form': form})
+            return self.render_to_response({"form": form})
 
 
 class ForgetPasswordEmailCode(View):
@@ -216,7 +224,7 @@ class ForgetPasswordEmailCode(View):
     def post(self, request: HttpRequest):
         form = ForgetPasswordCodeForm(request.POST)
         if not form.is_valid():
-            return HttpResponse("错误的邮箱")
+            return HttpResponse("Wrong email format")
         to_email = form.cleaned_data["email"]
 
         code = generate_code()
